c71670d89f94308944a0c3dbd31fd285483acc0d
hui <ihulufei@icloud.com>
2019-12-04 16:21:57 +0800

    Support pick individual day's solution to run

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::env;
+
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 
 fn main() {
-    println!("Day1 answer: {}", day1::answer());
-    println!("Day2 part 1 answer: {}", day2::part_1());
-    println!("Day2 part 2 answer: {}", day2::part_2());
-    println!("Day3 part 1 answer: {:?}", day3::part_1());
-    println!("Day3 part 2 answer: {:?}", day3::part_2());
-    println!("Day4 part 1 answer: {:?}", day4::part_1());
-    println!("Day4 part 2 answer: {:?}", day4::part_2());
+    let solution = env::args().nth(1).unwrap_or_default();
+
+    match solution.parse::<usize>() {
+        Ok(1) => println!("day1 answer: {}", day1::answer()),
+        Ok(21) => println!("{:?}", day2::part_1()),
+        Ok(22) => println!("{:?}", day2::part_2()),
+        Ok(31) => println!("{:?}", day3::part_1()),
+        Ok(32) => println!("{:?}", day3::part_2()),
+        Ok(41) => println!("{:?}", day4::part_1()),
+        Ok(42) => println!("{:?}", day4::part_2()),
+        _ => panic!("Invalid pick"),
+    }
 }
